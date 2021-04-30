bc965d1173ea3ef1ceafdf2a7b8bd08cb46e35f2
yenerpol <gomezyenner.unet@gmail.com>
2021-04-30 12:40:52 -0300

    avance

--- Scripts/EMSLIBS - Contest/PCA_exploratorio.R
+++ Scripts/EMSLIBS - Contest/PCA_exploratorio.R
@@ -23,9 +23,9 @@ data.preprocessing <- function(row1){
         # # (2) Hacer minimo = 0
         # min.val <- abs(min(new.row))
         # new.row <- new.row + min.val
-        # # (3) Normalizar por suma total - dismunuye el efecto matriz
-        # sum.total <- sum(new.row)
-        # new.row <- new.row/sum.total
+        # (3) Normalizar por suma total - dismunuye el efecto matriz
+        sum.total <- sum(new.row)
+        new.row <- new.row/sum.total
         new.row
 }
 
@@ -34,16 +34,17 @@ data.preprocessing <- function(row1){
 small.data.set <- apply(small.data.set, 1, data.preprocessing)
 small.data.set <- t(small.data.set) 
 
-small.data.set <- small.data.set %>% as_tibble() %>% mutate(class = trainClass.Data_1)
-
+small.data.set <- small.data.set %>% 
+        as_tibble() %>% 
+        mutate(class = trainClass.Data_1)
 
 # PCA ---------------------------------------------------------------------
 
-library("FactoMineR")
-library("factoextra")
+library("FactoMineR") # Performs PCA
+library("factoextra") # Visualize
 
-res.pca <- small.data.set %>% dplyr::select(-class) %>%  PCA(graph = FALSE)
-print(res.pca)
+set.seed(123)
+res.pca <- small.data.set %>% dplyr::select(-class) %>% PCA(graph = FALSE)
 
 fviz_pca_ind(res.pca,
              geom = "point", # show points only (nbut not "text")
@@ -53,3 +54,33 @@ fviz_pca_ind(res.pca,
              legend.title = "Classes"
          
 )
+
+# Eigenvalues / Variances
+eig.val <- get_eigenvalue(res.pca) %>% 
+        as_tibble() %>% 
+        rowid_to_column("Ncomp")
+colnames(eig.val)
+
+library(ggplot2)
+
+# Cumulative variance explained
+eig.val %>%  
+        ggplot(aes(x = Ncomp, y = cumulative.variance.percent)) + 
+        geom_point() +
+        geom_hline(aes(yintercept=95, color = "red")) +
+        ggtitle("Cumulative variance explained")
+
+# Scree plot
+fviz_eig(res.pca)
+
+sum(eig.val$eigenvalue > 1)     # 225 componentes tienen An eigenvalue > 1
+                                # Ademas, 225 comp explican el 96.5 de la varianza
+
+g.pca <- fviz_pca_ind(res.pca, geom.ind = "point", # show points only (nbut not "text")
+             col.ind = as_factor(trainClass.Data_1), # color by groups 
+             # palette = c("#000000","#E69F00","#56B4E9","#009E73","#F0E442","#0072B2",
+             #             "#D55E00","#CC79A7","#999999" ), 
+             addEllipses = TRUE, # Concentration ellipses 
+             legend.title = "Groups" )
+
+plotly::ggplotly(g.pca)

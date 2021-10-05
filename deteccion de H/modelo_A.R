c680485cf7dbeae8494c8ea6e0d65c2109e9a0dd
yenerpol <gomezyenner.unet@gmail.com>
2021-10-04 22:46:41 -0300

    update

--- deteccion de H/modelo_A.R
+++ deteccion de H/modelo_A.R
@@ -33,6 +33,7 @@ listaM.2 <- map(listaM.2, ~ .x %>% .[,2049:3983] %>%  data.frame() )
 names(listaM.1) <- c("2", "23", "42","79","99")
 names(listaM.2) <- c("2", "23", "42","79","99")
 
+rm(carpetas, data_path_1, data_path_2)
 # Preprocesado --------------------------------------------------------------
 # Primero reoordenar la matriz aleatoriamente
 FUN.random <- function(M){
@@ -45,7 +46,7 @@ FUN.random <- function(M){
 listaM.1 <- map(listaM.1, FUN.random)
 listaM.2 <- map(listaM.2, FUN.random)
 
-# Promediar 4 espectros, ahora tenemos 25 y 10
+# Promediar 3 espectros, ahora tenemos 33 y 13
 listaM.1 <- map(listaM.1, FUN.prom.4spec, i=3)
 listaM.2 <- map(listaM.2, FUN.prom.4spec, i=3)
 
@@ -57,6 +58,8 @@ listaM.2 <- map(listaM.2, ~ apply(.x, 1, FUN.norm.spec) %>% t() )
 listaM.1 <- map(listaM.1, ~ apply(.x, 1, FUN.sum.long, n = ncol(.x)/3 ) %>% t() )
 listaM.2 <- map(listaM.2, ~ apply(.x, 1, FUN.sum.long, n = ncol(.x)/3 ) %>% t() )
 
+# inspeccion
+# remover linea base
 L.1 <- map(listaM.1, ~ .x %>% apply(1, BaseLine, w= 25) )
 L.1[[1]] %>% plot.comparison() %>% plotly::ggplotly()
 
@@ -64,8 +67,8 @@ L.2 <- map(listaM.2, ~ .x %>% apply(1, BaseLine, w= 25) )
 L.2[[1]] %>% plot.comparison() %>% plotly::ggplotly()
 
 # Crear nuevas caracteristicas ----------------------------------------------------------------
-# Ahora que tengo los datos de la linea base, lo uso para crear las caracteristicas 
-# de interes para el analisis. 
+# Ahora que tengo los datos de la linea base, lo uso para crear las características 
+# de interés para el análisis. 
 # - Primero obtener a ojo un factor Z para desplazar la linea base y filtrar picos
 
 I_raw <- L.1 %>% 
@@ -104,14 +107,8 @@ BL.z <- BL * z
 indices <- I_raw > BL.z
 colnames(indices) <- c('a','b','c','d','f')
 indices <- indices %>% as.data.frame() %>% mutate(index = ifelse((a == TRUE) | (b  == TRUE) | (c  == TRUE) | (d  == TRUE) | (f  == TRUE), TRUE, FALSE)) 
-# sum(indices$a) # 181
-# sum(indices$b) # 207
-# sum(indices$c) # 205
-# sum(indices$d) # 209
-# sum(indices$f) # 197
-# sum(indices$g) # 204
 
-sum(indices$index) # 214
+sum(indices$index) # 246
 
 # Seleccionando características de interés
 I_new.1 <- L.1 %>% 
@@ -123,7 +120,10 @@ I_new.1 <- L.1 %>%
 df_temp <- I_new.1 %>% select(V1:V645)
 df_temp <- df_temp[,indices$index]
 
-data.1 <- data.frame(class = I_new.1$class, df_temp )
+data.1 <- data.frame(class = I_new.1$class, df_temp ) # 13 * 5 = 65 rows
+data.1.train <- data.1[rep(c(rep(TRUE,9), rep(FALSE,2), rep(FALSE,2)),5),]
+data.1.val <-   data.1[rep(c(rep(FALSE,9), rep(TRUE,2), rep(FALSE,2)),5),]
+data.1.test <-  data.1[rep(c(rep(FALSE,9), rep(FALSE,2), rep(TRUE,2)),5),]
 
 ###
 
@@ -137,31 +137,40 @@ df_temp <- I_new.2 %>% select(V1:V645)
 df_temp <- df_temp[,indices$index]
 
 data.2 <- data.frame(class = I_new.2$class, df_temp )
+data.2.train <- data.2[rep(c(rep(TRUE,25), rep(FALSE,4), rep(FALSE,4)),5),]
+data.2.val <-   data.2[rep(c(rep(FALSE,25), rep(TRUE,4), rep(FALSE,4)),5),]
+data.2.test <-  data.2[rep(c(rep(FALSE,25), rep(FALSE,4), rep(TRUE,4)),5),]
 
-rm(list= ls()[!(ls() %in% c('data.1','data.2'))])
+# Uniendo los datos
+train <- rbind(data.1.train,data.2.train)
+val <- rbind(data.1.val, data.2.val)
+test <- rbind(data.1.test,data.2.test) 
 
+rm(list= ls()[!(ls() %in% c('train','val','test'))])
 
-# AutoML ------------------------------------------------------------------
+save(train, val, test, file = './Data/Data_modelo_A.RData')
+# AutoML clasificacion ------------------------------------------------------
 set.seed(123)
 
 # distribucion aleatoria de las observaciones (rows)
-data.2 <- data.2[sample(1:nrow(data.2)), ]
-
-
-train <- data.2
-test <- data.1
-
+train <- train[sample(1:nrow(train)), ]
+val <- val[sample(1:nrow(val)), ]
+        
 table(train$class)
+table(test$class)
 table(test$class)
 
 library(h2o)
-h2o.init()
+h2o.init(nthreads = 4)
 
 # Convert your dataframes to h2o objects
+
 train <- as.h2o(train)
+val <- as.h2o(val)
 test <- as.h2o(test)
 
 train[,'class'] <- as.factor(train[,'class'])
+val[,'class'] <- as.factor(val[,'class'])
 test[,'class'] <- as.factor(test[,'class'])
 
 # Set variable names
@@ -176,12 +185,104 @@ models <- h2o.automl(x, y,
                      leaderboard_frame = test,
                      stopping_metric = "misclassification",
                      seed = 12345, 
-                     exclude_algos = c("GLM", "DRF", "XGBoost", "GBM", "StackedEnsemble"))
+                     include_algos = c("GLM", "DeepLearning", "DRF"))
 
 # Chossing the best model (relative to the stopping metric)
-best_model <- models@leader
+best_model.C <- models@leader
 show(best_model)
+
 # Prepare new data and forecasting
-new_data <- as.h2o(test)
-predictions <- h2o.predict(best_model, new_data)
+predictions <- h2o.predict(best_model, val)
 show(predictions)
+
+real.class <- as.data.frame(val[,'class']) 
+pred.class <- as.data.frame(predictions)
+
+sum(real.class$class != pred.class$predict) # Solo 1 mal!!!
+
+# save the model
+model_path <- h2o.saveModel(object = best_model, path = "./outputs", force = TRUE)
+print(model_path)
+# load the model
+model_path <- "C:\\Users\\gomez\\Documents\\LIBS\\Outputs\\DeepLearning_grid__2_AutoML_20211001_172201_model_31"
+saved_model <- h2o.loadModel(model_path)
+
+
+# AutoML Regression ------------------------------------------------------
+set.seed(123)
+
+# distribucion aleatoria de las observaciones (rows)
+train <- train[sample(1:nrow(train)), ]
+val <- val[sample(1:nrow(val)), ]
+test <- val[sample(1:nrow(test)), ]
+
+table(train$class)
+table(test$class)
+table(test$class)
+
+train$class <- as.numeric(train$class) 
+val$class <- as.numeric(val$class) 
+test$class <- as.numeric(test$class) 
+
+
+library(h2o)
+h2o.init(nthreads = 4)
+
+# Convert your dataframes to h2o objects
+
+train <- as.h2o(train)
+val <- as.h2o(val)
+test <- as.h2o(test)
+
+
+# Set variable names
+y <- "class"
+x <- setdiff(names(train), y)
+
+# Automatic model training
+# You can force Deep Learning by excluding the other algorithms with exclude_algos parameter
+models <- h2o.automl(x, y, 
+                     training_frame = train, 
+                     validation_frame = val, 
+                     leaderboard_frame = val,
+                     stopping_metric = "deviance",
+                     seed = 12345, 
+                     include_algos = c("GLM", "DeepLearning"))
+
+lb <- h2o.get_leaderboard(object = models, extra_columns = "ALL")
+lb <- lb %>% as.data.frame()
+
+# Chossing the best model (relative to the stopping metric)
+mdl1 <- models@leader
+mdl2 <- h2o.getModel(lb$model_id[2])
+mdl3 <- h2o.getModel(lb$model_id[3])
+
+show(best_model)
+
+predictions <- h2o.predict(best_model, test)
+show(predictions)
+
+real.ppm <- as.data.frame(test[,'class']) 
+mdl1.ppm <- as.data.frame(h2o.predict(mdl1, test))
+mdl2.ppm <- as.data.frame(h2o.predict(mdl2, test))
+mdl3.ppm <- as.data.frame(h2o.predict(mdl3, test))
+
+error <- data.frame(real.ppm, mdl1.ppm, mdl2.ppm, mdl3.ppm) %>% 
+        mutate(diff1 = (class - predict)) %>% 
+        mutate(diff2 = (class - predict.1)) %>% 
+        mutate(diff3 = (class - predict.2))
+
+
+error %>% map(summary)
+
+# save the model
+model_path_1 <- h2o.saveModel(object = mdl1, path = "./outputs", force = TRUE) 
+# "C:\\Users\\gomez\\Documents\\LIBS\\deteccion de H\\outputs\\DeepLearning_grid__3_AutoML_20211004_154141_model_8"
+model_path_2 <- h2o.saveModel(object = mdl2, path = "./outputs", force = TRUE)
+# "C:\\Users\\gomez\\Documents\\LIBS\\deteccion de H\\outputs\\DeepLearning_grid__3_AutoML_20211004_154141_model_3"
+model_path_3 <- h2o.saveModel(object = mdl3, path = "./outputs", force = TRUE)
+# "C:\\Users\\gomez\\Documents\\LIBS\\deteccion de H\\outputs\\DeepLearning_grid__2_AutoML_20211004_154141_model_8"
+
+
+saved_model <- h2o.loadModel(model_path)
+
